b93d695e03d3a8f6f0fd1cd75996637893f094fc
rinaeshin <rinaeshin@gmail.com>
2026-06-14 12:31:14 +0900

    refactor: SupplementaryScenarioDto에 builder 패턴 추가

--- backend/src/main/java/com/yeonam/tester/dto/SupplementaryScenarioDto.java
+++ backend/src/main/java/com/yeonam/tester/dto/SupplementaryScenarioDto.java
@@ -26,4 +26,39 @@ public class SupplementaryScenarioDto {
 
     public String getPriority() { return priority; }
     public void setPriority(String priority) { this.priority = priority; }
+
+    public static SupplementaryScenarioDtoBuilder builder() {
+        return new SupplementaryScenarioDtoBuilder();
+    }
+
+    public static class SupplementaryScenarioDtoBuilder {
+        private String testCaseName;
+        private String testScenario;
+        private String expectedResult;
+        private String priority;
+
+        public SupplementaryScenarioDtoBuilder testCaseName(String testCaseName) {
+            this.testCaseName = testCaseName;
+            return this;
+        }
+
+        public SupplementaryScenarioDtoBuilder testScenario(String testScenario) {
+            this.testScenario = testScenario;
+            return this;
+        }
+
+        public SupplementaryScenarioDtoBuilder expectedResult(String expectedResult) {
+            this.expectedResult = expectedResult;
+            return this;
+        }
+
+        public SupplementaryScenarioDtoBuilder priority(String priority) {
+            this.priority = priority;
+            return this;
+        }
+
+        public SupplementaryScenarioDto build() {
+            return new SupplementaryScenarioDto(testCaseName, testScenario, expectedResult, priority);
+        }
+    }
 }
